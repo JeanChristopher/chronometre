7ddedf847253eb3da24070ada5c0ed7187fe370b
Jean <jean-christopher-akpe.amany.1@ulaval.ca>
2019-09-28 15:29:36 +0200

    Add event on start / stop and reset button

---
+++ index.js
@@ -0,0 +1,33 @@
+var time = 0;
+var interval;
+var started = false ;
+
+setUp();
+
+function countDown(){
+        interval = setInterval(function(){
+            time++;
+            document.getElementById("compteur").innerHTML = time;
+        },1000)
+        started = true;
+}
+
+function setUp (){
+
+    document.getElementById('reset').addEventListener('click', function(){
+        location.reload();
+    });
+
+    document.getElementById('start').addEventListener('click', function(){
+    
+    if (started == false){          
+        countDown();
+    } else{
+        clearInterval(interval);
+        started = false ;
+    }
+    
+    })     
+       
+       
+}
